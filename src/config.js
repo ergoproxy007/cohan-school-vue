ec799e774729faf35497cc93d9212347c7793261
daniel.torres <daniel.torres@zabud.com.co>
2025-10-23 14:59:38 -0500

    doc: set environment

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ export const NODE_ENV = 'dev';
 
 export const API_BASE_URL = (() => {
   if (NODE_ENV === 'production') {
-    return 'https://api.miapp.com/api';
-  } else if (NODE_ENV === 'staging') {
-    return 'https://staging.api.miapp.com/api';
+    return 'https://gs8af4pd2b.execute-api.us-east-2.amazonaws.com/dev/api';
+  } else if (NODE_ENV === 'dev') {
+    return 'https://gs8af4pd2b.execute-api.us-east-2.amazonaws.com/dev/api';
   } else {
     return 'http://localhost:8080/api';
   }
